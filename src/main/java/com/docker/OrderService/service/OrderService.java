f47cf6e58cd336a5763877062bc7421b30138caa
unknown <sonia8930@gmail.com>
2022-09-26 16:11:03 +0530

    Config sever changes

--- src/main/java/com/docker/OrderService/service/OrderService.java
+++ src/main/java/com/docker/OrderService/service/OrderService.java
@@ -6,24 +6,32 @@ import com.docker.OrderService.common.TransactionResponse;
 import com.docker.OrderService.entity.Order;
 import com.docker.OrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
 
     @Autowired
+    @Lazy
     private RestTemplate restTemplate;
+
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String paymentEndpoint;
 
     public TransactionResponse saveOrder(TransactionRequest request) {
         Order order = request.getOrder();
         Payment payment = request.getPayment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
-        Payment paymentResponse = restTemplate.postForObject("http://payment-service/payment/doPayment",payment, Payment.class);
+        Payment paymentResponse = restTemplate.postForObject(paymentEndpoint,payment, Payment.class);
         String response = paymentResponse.getPaymentStatus().equals("success") ? "payment processing successfull and order placed" : "there is a failure in payment api, order added to cart";
         orderRepository.save(order);
 
